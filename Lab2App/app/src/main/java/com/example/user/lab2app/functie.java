d43790a13e967bfd4d5565753aa9f0125703e49d
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-11-11 21:07:40 -0800

    delete all the trash

--- Lab2App/app/src/main/java/com/example/user/lab2app/functie.java
+++
@@ -1,5 +0,0 @@
-package com.example.user.lab2app;
-
-public interface functie {
-    public String calcul(String stare,String intrare);
-}
--- Lab2App/app/src/main/java/com/example/user/lab2app/gc.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.lab2app;
-
-public class gc implements functie {
-    public String calcul(String stare, String intrare) {
-// Implementare prin swich
-         return intrare.toUpperCase();
-    }
-
-}
