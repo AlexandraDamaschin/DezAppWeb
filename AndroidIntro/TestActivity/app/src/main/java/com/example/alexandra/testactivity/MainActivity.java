4bb26516f37ad81e640f475587a839abaa71f6f5
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-11-11 22:47:47 -0800

    states of android app done

---
+++ AndroidIntro/TestActivity/app/src/main/java/com/example/alexandra/testactivity/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.alexandra.testactivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tToast("Starea curenta: onCreate().");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void onStart() {
+        super.onStart();
+        tToast("Starea curenta: onStart().");
+    }
+
+    public void onRestart() {
+        super.onRestart();
+        tToast("Starea curenta: onRestart().");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        tToast("Starea curenta: onResume().");
+    }
+
+    public void onPause() {
+        super.onPause();
+        tToast("Starea curenta: onPause().");
+    }
+
+    public void onStop() {
+        super.onStop();
+        tToast("Starea curenta: onStop().");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        tToast("Starea curenta: onDistroy().");
+    }
+
+    private void tToast(String str) {
+        Context context = getApplicationContext();
+        int time = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, str, time);
+        toast.show();
+    }
+}
