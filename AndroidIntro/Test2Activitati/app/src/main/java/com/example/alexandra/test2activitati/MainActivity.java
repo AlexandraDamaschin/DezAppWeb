7ed485e05dc886a3a3fba67d5ecede1ce0980dcc
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-12-30 18:29:51 +0200

    sends from first activity to second activity

--- AndroidIntro/Test2Activitati/app/src/main/java/com/example/alexandra/test2activitati/MainActivity.java
+++ AndroidIntro/Test2Activitati/app/src/main/java/com/example/alexandra/test2activitati/MainActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    String editText = "";
+    EditText editText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText1 = (EditText) findViewById(R.id.editText1);
     }
-    public  void next(View v){
+
+    public void next(View v) {
+
+        editText = editText1.getText().toString();
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("EditText1", editText);
         startActivity(intent);
     }
 }
--- AndroidIntro/Test2Activitati/app/src/main/java/com/example/alexandra/test2activitati/SecondActivity.java
+++ AndroidIntro/Test2Activitati/app/src/main/java/com/example/alexandra/test2activitati/SecondActivity.java
@@ -3,7 +3,9 @@ package com.example.alexandra.test2activitati;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class SecondActivity extends Activity {
 
@@ -12,6 +14,11 @@ public class SecondActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        String getEditText = getIntent().getStringExtra("EditText1");
+        Log.e("editText1", getEditText);
+
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        editText2.setText(getEditText);
     }
 
     public void back(View v) {
