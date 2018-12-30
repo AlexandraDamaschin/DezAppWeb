b15ee22eebeb7566e2889cf1324d4d8dd8786fc2
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-12-30 19:53:27 +0200

    services exercise done

--- AndroidIntro/StartStopService/app/src/main/java/com/example/alexandra/startstopservice/StartService.java
+++ AndroidIntro/StartStopService/app/src/main/java/com/example/alexandra/startstopservice/StartService.java
@@ -6,59 +6,44 @@ import android.os.IBinder;
 import android.widget.Toast;
 
 public class StartService extends Service {
-    /**
-     * indicates how to behave if the service is killed
-     */
+
+    // indicates how to behave if the service is killed
     int mStartMode;
 
-    /**
-     * interface for clients that bind
-     */
+    // interface for clients that bind
     IBinder mBinder;
 
-    /**
-     * indicates whether onRebind should be used
-     */
+    //indicates whether onRebind should be used
     boolean mAllowRebind;
 
-    /**
-     * Called when the service is being created.
-     */
+    // Called when the service is being created.
     @Override
     public void onCreate() {
 
     }
 
-    /**
-     * The service is starting, due to a call to startService()
-     */
+    //The service is starting, due to a call to startService()
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "Start service onStartCommand", Toast.LENGTH_SHORT).show();
         return mStartMode;
     }
 
-    /**
-     * A client is binding to the service with bindService()
-     */
+    //A client is binding to the service with bindService()
     @Override
     public IBinder onBind(Intent intent) {
         Toast.makeText(this, "Start service onBind", Toast.LENGTH_SHORT).show();
         return mBinder;
     }
 
-    /**
-     * Called when all clients have unbound with unbindService()
-     */
+    //Called when all clients have unbound with unbindService()
     @Override
     public boolean onUnbind(Intent intent) {
         Toast.makeText(this, "Start service onUnbind", Toast.LENGTH_SHORT).show();
         return mAllowRebind;
     }
 
-    /**
-     * Called when The service is no longer used and is being destroyed
-     */
+    //Called when The service is no longer used and is being destroyed
     @Override
     public void onDestroy() {
         super.onDestroy();
