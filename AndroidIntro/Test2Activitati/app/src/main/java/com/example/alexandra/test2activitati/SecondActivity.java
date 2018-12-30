ce65e61922b096b30d9ceaf7f1e29dee6205c3b8
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-12-30 15:57:57 +0200

    second activity

---
+++ AndroidIntro/Test2Activitati/app/src/main/java/com/example/alexandra/test2activitati/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.alexandra.test2activitati;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
