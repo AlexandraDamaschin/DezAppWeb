7b701316e652e5565ab8e531f2720da59ce44546
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-12-31 18:24:48 +0200

    service need to be implemented

--- AndroidIntro/HashSetAB/app/src/main/java/com/example/alexandra/hashsetab/HashSetService.java
+++ AndroidIntro/HashSetAB/app/src/main/java/com/example/alexandra/hashsetab/HashSetService.java
@@ -3,14 +3,49 @@ package com.example.alexandra.hashsetab;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class HashSetService extends Service {
-    public HashSetService() {
+    // indicates how to behave if the service is killed
+    int mStartMode;
+
+    // interface for clients that bind
+    IBinder mBinder;
+
+    //indicates whether onRebind should be used
+    boolean mAllowRebind;
+
+    // Called when the service is being created.
+    @Override
+    public void onCreate() {
+
     }
 
+    //The service is starting, due to a call to startService()
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Start service", Toast.LENGTH_SHORT).show();
+        return mStartMode;
+    }
+
+    //A client is binding to the service with bindService()
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Toast.makeText(this, "Start service onBind", Toast.LENGTH_SHORT).show();
+        return mBinder;
+    }
+
+    //Called when all clients have unbound with unbindService()
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Toast.makeText(this, "Start service onUnbind", Toast.LENGTH_SHORT).show();
+        return mAllowRebind;
+    }
+
+    //Called when The service is no longer used and is being destroyed
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
     }
 }
--- AndroidIntro/HashSetAB/app/src/main/java/com/example/alexandra/hashsetab/MainActivity.java
+++ AndroidIntro/HashSetAB/app/src/main/java/com/example/alexandra/hashsetab/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.alexandra.hashsetab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), HashSetService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), HashSetService.class));
+    }
+
+    // Union A and B
+    public void unionService(View view) {
+        startService(new Intent(getBaseContext(), HashSetService.class));
+    }
+
+    // Intersect A and B
+    public void intersectionService(View view) {
+        startService(new Intent(getBaseContext(), HashSetService.class));
+    }
+
+    // Difference between A and B
+    public void differenceService(View view) {
+        startService(new Intent(getBaseContext(), HashSetService.class));
+    }
+
+    // Symmetric Difference between A and B
+    public void symmetricDifference(View view) {
+        startService(new Intent(getBaseContext(), HashSetService.class));
     }
 }
