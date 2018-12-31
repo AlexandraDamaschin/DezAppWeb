457993d8d7bb9e4ec86a6a5596d9acbf9cfe1a61
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-12-31 17:25:57 +0200

    start stop service buttons

---
+++ AndroidIntro/HashSetAB/app/src/main/java/com/example/alexandra/hashsetab/HashSetService.java
@@ -0,0 +1,16 @@
+package com.example.alexandra.hashsetab;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class HashSetService extends Service {
+    public HashSetService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
