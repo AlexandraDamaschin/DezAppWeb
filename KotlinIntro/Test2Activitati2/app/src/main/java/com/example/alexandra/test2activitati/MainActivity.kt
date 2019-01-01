43555a7c67cd4848cadd68dddc9faaa44e58771d
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-01-01 14:35:23 +0200

    2 activitati sends and receives kotlin done

--- KotlinIntro/Test2Activitati2/app/src/main/java/com/example/alexandra/test2activitati/MainActivity.kt
+++ KotlinIntro/Test2Activitati2/app/src/main/java/com/example/alexandra/test2activitati/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.alexandra.test2activitati
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var editText = ""
+    private lateinit var editText1: EditText
+    private var getEditText: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText1 = findViewById<View>(R.id.editText1) as EditText
+
+        getEditText = intent.getStringExtra("EditText2")
+
+        editText1.setText(getEditText)
+    }
+
+    fun next(v: View) {
+        editText = editText1.text.toString()
+        val intent = Intent(this@MainActivity, SecondActivity::class.java)
+        intent.putExtra("EditText1", editText)
+        startActivity(intent)
     }
 }
--- KotlinIntro/Test2Activitati2/app/src/main/java/com/example/alexandra/test2activitati/SecondActivity.kt
+++ KotlinIntro/Test2Activitati2/app/src/main/java/com/example/alexandra/test2activitati/SecondActivity.kt
@@ -1,12 +1,33 @@
 package com.example.alexandra.test2activitati
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class SecondActivity : AppCompatActivity() {
+
+    private var editText = ""
+    private lateinit var editText2: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        editText2 = findViewById<View>(R.id.editText2) as EditText
+
+        val getEditText = intent.getStringExtra("EditText1")
+
+        if (getEditText != null) {
+            editText2.setText(getEditText)
+        }
+    }
+
+    fun back(v: View) {
+        editText = editText2.text.toString()
+        val intent = Intent(this@SecondActivity, MainActivity::class.java)
+        intent.putExtra("EditText2", editText)
+        startActivity(intent)
     }
 }
