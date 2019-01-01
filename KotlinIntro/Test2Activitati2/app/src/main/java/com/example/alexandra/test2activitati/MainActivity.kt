7428c714807695677a5073ca73a2881a1e0a49fe
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-01-01 13:37:22 +0200

    kotlin file

---
+++ KotlinIntro/Test2Activitati2/app/src/main/java/com/example/alexandra/test2activitati/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alexandra.test2activitati
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
